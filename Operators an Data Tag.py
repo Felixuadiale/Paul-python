ff550a5fc75def325d7a9c9c1e83ff1fae5a2dbe
Felixuadiale <kennedoc@gmail.com>
2024-11-16 08:59:42 +0100

    Create Operators an Data Tag.py

---
+++ Operators an Data Tag.py
@@ -0,0 +1,12 @@
+name='penguin'
+age=15
+isstudent=True
+weight=38.5
+print("name",name)
+print("age",age)
+print("isstudent",isstudent)
+print("weight",weight)
+print('the data type of name variable is',type(name))
+print('the data type of isstudent variable is',type(isstudent))
+print('the data type of age variable is',type(age))
+print('the data type of weight variable is',type(weight))
