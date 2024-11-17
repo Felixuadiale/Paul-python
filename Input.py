1b690ff6a16d1a00f79eac7e6afce757de8255c8
Felixuadiale <kennedoc@gmail.com>
2024-11-17 12:49:56 +0100

    k

---
+++ Input.py
@@ -0,0 +1,4 @@
+name=input("Enter name")
+print('Good Morning',name)
+age=int(input('Enter Age'))
+print(type(age))
---
+++ Turtle Pen.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("Green")
+board=turtle.Turtle()
+#Triangle
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+turtle.done()
