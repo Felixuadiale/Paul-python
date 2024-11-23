d5b93066c7ad37540144df04aee4e2720368ab4e
Felixuadiale <kennedoc@gmail.com>
2024-11-23 09:01:49 +0100

    k
    k

---
+++ OddEven.py
@@ -0,0 +1,6 @@
+number=int(input("Enter Number to Check"))
+print("Number to be checked  :", number)
+if number%2==0:
+    print("This is a even number")
+else:
+    print("This is a odd number")
---
+++ Work with list.py
@@ -0,0 +1,3 @@
+list=["mango","guava","apple"]
+print ("Length of list:", len(list))
+print ("First element:", list[0])
