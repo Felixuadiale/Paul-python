953a45bbd3b186e855a59d7050b9b58b7fc8a90d
Felixuadiale <kennedoc@gmail.com>
2024-12-01 12:02:40 +0100

    Update Parrot Bird.py

--- Parrot Bird.py
+++ Parrot Bird.py
@@ -4,7 +4,14 @@ class parrot:
         self.name=name
         self.age=age
 
+    def sing(self,song):
+        return f'{self.name} sings {song}'
+    def dance(self):
+        return f'{self.name} is now dancing'
+
 blue=parrot('Blue',10)
 wool=parrot('Wool',15)
 print('Blue is a',blue.species)
 print(blue.name,' is',blue.age,'years old')
+print(blue.sing('hAPPY'))
+print(blue.dance)
